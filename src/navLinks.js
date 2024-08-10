3813726a79f1d37bcb9dedb1afa585b2262c8c3e
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-10 15:01:55 -0700

    feat(navigation links collection): :sparkles: introduce collection for nav links

---
+++ src/navLinks.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'Login', path: '/login' },
+  { name: 'Register', path: '/register' },
+  { name: 'Check Out', path: '/checkout' },
+];
