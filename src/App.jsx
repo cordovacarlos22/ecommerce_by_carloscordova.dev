0bf2eaebb17ef23abc53b79daf8d7410a00ba2ca
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-10 15:00:21 -0700

    feat(routes): :truck: introduces routes to : home,login,register,checkout

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,36 @@ import React from 'react'
 import { ProductsProvider } from './context/useProductsContext'
 import Nav from './components/Nav'
 import Home from './pages/Home'
+import OffertSpan from './components/OffertSpan'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './components/ErrorPage'
+import Login from './components/Login'
+import Register from './components/Register'
+import CheckOut from './components/CheckOut'
 
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Nav></Nav>,
+     
+      errorElement: <ErrorPage />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/login', element: <Login /> },
+        { path: '/register', element: <Register /> },
+        { path: '/checkout', element: <CheckOut /> },
+      ],
+    }
+  ])
   return (
     <>
       <ProductsProvider>
-        <Nav />
-        <Home/>
-        {/* Your application components */}
-
-
-    </ProductsProvider>
+        <OffertSpan/>
+        <RouterProvider router={router} />
+      </ProductsProvider>
     </>
   )
 }
