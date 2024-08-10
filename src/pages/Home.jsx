cca51097aad6e0ed4b688e222ae3f970abdc02d9
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-10 10:04:11 -0700

    feat(Home): :sparkles: added loading contidional and separed Card Componet
    introduced loading skeleton Component and separated Cards items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import { ProductsContext } from '@/context/useProductsContext'
+import React, { useContext } from 'react'
+import Card from '@/components/Card';
+import SkeletonCard from '@/components/SkeletonCard';
 const Home = () => {
+  const productsArray = useContext(ProductsContext);
+
+  const filterProducts = productsArray.products.filter((product) => {
+    return product.product_name.toLowerCase().includes(productsArray.searchTerm.toLowerCase());
+  });
+  // Dynamically create an array with 10 items (or any number you want)
+  const skeletons = Array.from({ length: 52 });
+
   return (
-    <div>Home</div>
+    <main className='flex  flex-wrap justify-center   items-center w-scree gap-2'>
+      {
+        productsArray.loading ?
+          (
+            <article className='flex flex-wrap justify-center items-center w-full gap-2'>
+              {skeletons.map((_, index) => (
+                <SkeletonCard key={index} />
+              ))}
+            </article>
+          ) :
+          (<>
+            {
+              filterProducts && filterProducts.length > 0 ? (
+                <>
+
+                  {filterProducts && filterProducts.map((item) => {
+                    return (
+
+                      <article className='flex flex-wrap justify-center items-center   gap-2' key={item.product_name}>
+                        <Card
+                          key={item.product_name}
+                          url={item.url}
+                          image={item.image}
+                          product_name={item.product_name}
+                        />
+                      </article>
+                    )
+                  })
+                  }
+                </>
+              ) : (
+                <>
+                  <h1>
+                    No products found. Please enter a product name in the search bar.
+                  </h1>
+                </>
+              )
+            }
+          </>
+          )
+
+      }
+    </main>
   )
-}
+};
 
 export default Home
