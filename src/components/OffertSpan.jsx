b4be650f83a0d83a06458a7a0962087d65953750
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-10 15:07:17 -0700

    feat(offers): :sparkles: introduce offerts conponent

---
+++ src/components/OffertSpan.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const OffertSpan = () => {
+  return (
+    <aside className='bg-blue-400 text-center'>
+
+      <h2
+      className=' text-sm md:text-lg font-bold underline text-white'
+      >
+        FREE STANDARD SHIPPING ON ORDER OF $50+
+      </h2>
+    </aside>
+  )
+}
+
+export default OffertSpan
