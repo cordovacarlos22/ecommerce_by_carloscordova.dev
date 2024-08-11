202043cbde23a464ae2e10a80cd440ed0728632d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-11 11:03:30 -0700

    refactor(APP): :truck: update imports to 5 components

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Nav from './components/Nav'
 import Home from './pages/Home'
 import OffertSpan from './components/OffertSpan'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ErrorPage from './components/ErrorPage'
-import Login from './components/Login'
-import Register from './components/Register'
-import CheckOut from './components/CheckOut'
+import ErrorPage from './pages/ErrorPage'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import CheckOut from './pages/CheckOut'
+import ProductDetail from './components/ProductDetail'
 
 
 const App = () => {
@@ -16,20 +17,21 @@ const App = () => {
     {
       path: '/',
       element: <Nav></Nav>,
-     
+
       errorElement: <ErrorPage />,
       children: [
         { path: '/', element: <Home /> },
         { path: '/login', element: <Login /> },
         { path: '/register', element: <Register /> },
         { path: '/checkout', element: <CheckOut /> },
+        { path: '/product/:id', element: <ProductDetail /> },
       ],
     }
   ])
   return (
     <>
       <ProductsProvider>
-        <OffertSpan/>
+        <OffertSpan />
         <RouterProvider router={router} />
       </ProductsProvider>
     </>
