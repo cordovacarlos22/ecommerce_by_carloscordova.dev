9d47d7cd8d9eb4abdaff7652681fe0863e448e6d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-14 12:52:02 -0700

    feat(Card): :sparkles: add to card btn is not disable is user not login.

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import shoppinCartIcon from './../assets/cart-plus.svg'
+import { userContext } from '@/context/UserContext'
+import { Link } from 'react-router-dom'
 function ShoppingCart({ className, imgWidth }) {
+
+  const { token } = useContext(userContext)
+  
+  const handleCart = () => {
+    if (!token) {
+      alert('Please login to add items to the cart')
+    } else {
+      alert('items added successfully')
+      // navigate to cart page or login page if not logged in
+    }
+  }
   return (
-    <button
+    <Link 
+      to={token? null:'/login'}
       className={className}
+      onClick={handleCart}
     >
       <img
 
@@ -11,7 +26,7 @@ function ShoppingCart({ className, imgWidth }) {
         src={shoppinCartIcon}
         alt="Shopping cart icon" />
       Add to Cart
-    </button>
+    </Link>
   )
 }
 
