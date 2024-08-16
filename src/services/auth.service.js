9b23219ef2c60ccdcac73761d68280a10e69165d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-16 07:11:09 -0700

    feat(services): :zap: change login to loginUserService function to improve performance

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,15 +2,7 @@ import axios from "axios";
 
 let url = import.meta.env.VITE_DB_URL
 
-export const login = async (data) => {
-  // Your login logic goes here
-  try {
-    const response = await axios.post(`${url}/login`, data);
-    return response
-  } catch (error) {
-    console.error(error);    
-  }
-}
+export const loginUserService = async (data) => axios.post(`${url}/login`, data);
 
 export const registerUserService = (data) => axios.post(`${url}/register`, data)
 
