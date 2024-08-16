c73611e801429dd2613f5d1c4b2c95a2924063e7
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-16 06:52:07 -0700

    feat(services): :zap: update signup now performing better.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-import { Navigate } from "react-router-dom";
-import {  toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 let url = import.meta.env.VITE_DB_URL
 
 export const login = async (data) => {
@@ -14,48 +12,9 @@ export const login = async (data) => {
   }
 }
 
-export const signup = async (data) => {
-
-  // Your signup logic goes here
-  try {
-    toast.info(' waiting for server !', {
-      position: "bottom-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light"
-    });
-    const response = await axios.post(`${url}/register`, data);
-    // todo : should add a toasify alert if user created 
-    toast.success(' you have registed !', {
-      position: "bottom-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light"
-    });
-   
-    return response.data;
-  } catch (error) {
-    //todo :   toastify error message 
-    toast.error(' sorry email already in use', {
-      position: "bottom-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light"
-    });
+export const registerUserService = (data) => axios.post(`${url}/register`, data)
 
 
-  }
-};
+
+
 
