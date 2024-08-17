bfa413c0ec5121eaa7c2a526e7d78292ac7e9589
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-17 09:22:23 -0700

    feat(dashboard): :sparkles: update component to render components base on its case by using a switch

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import CreateItem from "@/components/CreateItem";
+import React, { useState } from "react";
+
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [activeComponent, setActiveComponent] = useState("home");
 
-export default Dashboard
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "createItem":
+        return <CreateItem />;
+      default:
+        return (
+          <>
+            <div className="font-bold">Welcome to your Dashboard</div>
+          </>
+        );
+    }
+  };
+
+  return (
+    <section className="flex flex-col md:flex-row  bg-gray-200  ">
+      <br />
+      <div className=" bg-blue-700 md:min-h-screen flex flex-col justify-center items-center gap-4 p-4">
+        <h1 className="font-bold underline text-white">Dashboard</h1>
+        <nav className="flex flex-col">
+          <button onClick={() => setActiveComponent("createItem")}>
+            Create Item
+          </button>
+          <button onClick={() => setActiveComponent("viewItems")}>
+            View Items
+          </button>
+          {/* Add more buttons for other components */}
+        </nav>
+
+      </div>
+      <div className="flex flex-col justify-center items-center m-auto">
+        {/* This will render the component based on the active state */}
+        {renderComponent()}
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
