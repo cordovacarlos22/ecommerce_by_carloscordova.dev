686fc4395f46646ef898e7ce529f6ba0b6aa20d5
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-17 09:19:17 -0700

    fix(services): :bug: FIX createItemService
    The issue with your createItemService function is in the structure of the axios.post request. Axios expects the data to be passed as the second argument and the configuration (like headers) to be passed as the third argument. In your code, you’re passing both data and headers in a single object, which isn’t correct.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,9 +2,18 @@ import axios from "axios";
 
 let url = import.meta.env.VITE_DB_URL
 
+
+
+
 export const loginUserService = async (data) => axios.post(`${url}/login`, data);
 
-export const registerUserService = (data) => axios.post(`${url}/register`, data)
+export const registerUserService = (data) => axios.post(`${url}/register`, data);
+
+export const createItemService = (token, data) => axios.post(`${url}/items`,data, {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
 
 
 
