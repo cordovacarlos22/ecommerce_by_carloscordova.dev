e2c4c814913b03156691397ff9b108ee257e9d5a
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-17 10:14:36 -0700

    feat(dashboard): :sparkles: introduce List all users

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import CreateItem from "@/components/CreateItem";
+import GetAllUsers from "@/components/GetAllUsers";
 import React, { useState } from "react";
+import { NavLink,Link } from "react-router-dom";
 
 
 const Dashboard = () => {
@@ -9,10 +11,20 @@ const Dashboard = () => {
     switch (activeComponent) {
       case "createItem":
         return <CreateItem />;
+      case "getAllUsers":
+        return <GetAllUsers />;
       default:
         return (
           <>
-            <div className="font-bold">Welcome to your Dashboard</div>
+            <div className="text-center" >
+
+              <h1
+                className="font-bold text-2xl"
+              >
+                Welcome to your Dashboard
+              </h1>
+              <span>Select one of the options on the navigation menu</span>
+            </div>
           </>
         );
     }
@@ -22,14 +34,31 @@ const Dashboard = () => {
     <section className="flex flex-col md:flex-row  bg-gray-200  ">
       <br />
       <div className=" bg-blue-700 md:min-h-screen flex flex-col justify-center items-center gap-4 p-4">
-        <h1 className="font-bold underline text-white">Dashboard</h1>
-        <nav className="flex flex-col">
-          <button onClick={() => setActiveComponent("createItem")}>
+        <Link
+        to='/dashboard'
+        
+        >
+          <h1 className="font-bold  text-white">Dashboard</h1>
+        </Link>
+        <nav className="flex flex-col text-white ">
+          <button
+            className={` p-2 ${activeComponent === "createItem"
+                ? "text-blue-500 underline bg-white rounded"
+                : "text-white"
+              }`}
+            onClick={() => setActiveComponent("createItem")}>
             Create Item
           </button>
-          <button onClick={() => setActiveComponent("viewItems")}>
-            View Items
+
+          <button
+            className={` p-2 ${activeComponent === "getAllUsers"
+              ? "text-blue-500 underline bg-white rounded"
+              : "text-white"
+              }`}
+            onClick={() => setActiveComponent("getAllUsers")}>
+            list all  users
           </button>
+         
           {/* Add more buttons for other components */}
         </nav>
 
