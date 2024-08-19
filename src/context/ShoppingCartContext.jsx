9dcf0fb0cf47badeeb3b44f16a08de77ba73beb3
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-19 06:40:54 -0700

    feat(context): :sparkles: introduce cart functions

--- src/context/ShoppingCartContext.jsx
+++ src/context/ShoppingCartContext.jsx
@@ -1,15 +1,49 @@
-const { createContext } = require("react");
+const { createContext, useState } = require("react");
 
 const CartContext = createContext();
 
-const data = {
 
-};
 
 const CartProvider = ({ children }) => {
 
+  const [cartItems, setCartItems] = useState([])// cart items array 
 
 
+  const addToCart = () => {
+    
+  };
+
+  const removeFromCart = () => {
+    
+  };
+
+  const incrementQuantity = () => {
+    
+  };
+
+  const decrementQuantity = () => {
+    
+  };
+
+  const getTotalPrice = () => {
+    
+  };
+
+
+
+
+
+
+
+  const data = {
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    incrementQuantity,
+    decrementQuantity,
+    getTotalPrice,
+  };
   return (
     <CartContext.Provider value={data}>
       {children}
