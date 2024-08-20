ef882e0ed548a3f4115673d666d3fcf383714b13
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-19 17:48:10 -0700

    feat(checkout): :boom: checkout now working from context api.

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,27 +1,41 @@
+import { CartContext } from '@/context/ShoppingCartContext'
 import { userContext } from '@/context/UserContext'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 const CheckOut = () => {
   const navigate = useNavigate()
-  const { login } = useContext(userContext);
+
+  const { login } = useContext(userContext)
+  const {
+
+    cartItems,
+    removeFromCart,
+    incrementQuantity,
+    decrementQuantity,
+    getTotalPrice
+  } = useContext(CartContext);
+
+  useEffect(() => {
+
+
+    if (!login) {
+      setTimeout(() => {
+        navigate('/login');
+      }, 3000);
+    }
+  }, [login, navigate]);
 
   if (!login) {
-
-    setTimeout(() => {
-      navigate('/login')
-    }, 3000);
     return (
-
-      <aside
-        className='flex justify-center items-center w-screen min-h-screen'
-      >
-        <h1 className= 'capitalize'>
-          please login to Check Out
+      <aside className='flex justify-center items-center w-screen min-h-screen'>
+        <h1 className='capitalize'>
+          Please login to Check Out
         </h1>
       </aside>
-    )
+    );
   }
+
   return (
     <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
       <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
@@ -30,126 +44,123 @@ const CheckOut = () => {
         </h2>
         <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
           <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
-            {/* items container */}
             <div className="space-y-6">
-              {/*items  */}
-              <section className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
-                <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
-                  <Link to="#" className="shrink-0 md:order-1">
-                    <img
-                      className="h-20 w-20"
-                      src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg"
-                      alt="imac image"
-                    />
-
-                  </Link>
-                  <label htmlFor="counter-input" className="sr-only">
-                    Choose quantity:
-                  </label>
-                  <div className="flex items-center justify-between md:order-3 md:justify-end">
-                    <div className="flex items-center">
-                      {/* decrement btn */}
-                      <button
-                        type="button"
-                        id="decrement-button"
-                        data-input-counter-decrement="counter-input"
-                        className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
-                      >
-                        <svg
-                          className="h-2.5 w-2.5 text-gray-900 dark:text-white"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 18 2"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M1 1h16"
+              {/* Items */}
+              {cartItems.length > 0 ? (<>
+                {cartItems.map((item) => (
+                  <section key={item.id} className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
+                    <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                      <Link to={`/product/${item.id}`} className="shrink-0 md:order-1">
+                        <img
+                          className="h-20 w-20"
+                          src={item.image}
+                          alt={item.product_name}
+                        />
+                      </Link>
+                      <div className="flex items-center justify-between md:order-3 md:justify-end">
+                        <div className="flex items-center">
+                          {/* Decrement button */}
+                          <button
+                            type="button"
+                            onClick={() => decrementQuantity(item.id)}
+                            className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                          >
+                            <svg
+                              className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 18 2"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M1 1h16"
+                              />
+                            </svg>
+                          </button>
+                          {/* Quantity input */}
+                          <input
+                            type="text"
+                            id="counter-input"
+                            data-input-counter=""
+                            className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
+                            value={item.quantity}
+                            readOnly
                           />
-                        </svg>
-                      </button>
-                      {/* item value */}
-                      <input
-                        type="text"
-                        id="counter-input"
-                        data-input-counter=""
-                        className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
-                        placeholder=""
-                        defaultValue={2}
-                        required=""
-                      />
-                      <button
-                        type="button"
-                        id="increment-button"
-                        data-input-counter-increment="counter-input"
-                        className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
-                      >
-                        <svg
-                          className="h-2.5 w-2.5 text-gray-900 dark:text-white"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 18 18"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M9 1v16M1 9h16"
-                          />
-                        </svg>
-                      </button>
+                          {/* Increment button */}
+                          <button
+                            type="button"
+                            onClick={() => incrementQuantity(item.id)}
+                            className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                          >
+                            <svg
+                              className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 18 18"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M9 1v16M1 9h16"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                        <div className="text-end md:order-4 md:w-32">
+                          <p className="text-base font-bold text-gray-900 dark:text-white">
+                            ${item.price}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                        {/* Item title */}
+                        <p className="text-base font-medium text-gray-900 dark:text-white">
+                          {item.product_name}
+                        </p>
+                        <div className="flex items-center gap-4">
+                          <button
+                            type="button"
+                            className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
+                            onClick={() => removeFromCart(item.id)}
+                          >
+                            <svg
+                              className="me-1.5 h-5 w-5"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width={24}
+                              height={24}
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M6 18 17.94 6M18 18 6.06 6"
+                              />
+                            </svg>
+                            Remove
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                    <div className="text-end md:order-4 md:w-32">
-                      {/* item price */}
-                      <p className="text-base font-bold text-gray-900 dark:text-white">
-                        $1,499
-                      </p>
-                    </div>
-                  </div>
-                  <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
-                    {/* item title */}
-                    <a
-                      href="#"
-                      className="text-base font-medium text-gray-900 hover:underline dark:text-white"
-                    >
-                      PC system All in One APPLE iMac (2023) mqrq3ro/a, Apple M3,
-                      24" Retina 4.5K, 8GB, SSD 256GB, 10-core GPU, Keyboard layout
-                      INT
-                    </a>
-                    <div className="flex items-center gap-4">
-
-                      <button
-                        type="button"
-                        className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
-                      >
-                        <svg
-                          className="me-1.5 h-5 w-5"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width={24}
-                          height={24}
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M6 18 17.94 6M18 18 6.06 6"
-                          />
-                        </svg>
-                        Remove
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </section>
+                  </section>
+                ))}
+              </>
+              ) :
+                (
+                <>    
+                    <h1>no items in your cart</h1>
+                    <span>please add items</span>
+                </>)}
             </div>
           </div>
           <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
@@ -159,47 +170,16 @@ const CheckOut = () => {
               </p>
               <div className="space-y-4">
                 <div className="space-y-2">
-                  <dl className="flex items-center justify-between gap-4">
-                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                      Original price
+                  {/* Summary of prices */}
+                  <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
+                    <dt className="text-base font-bold text-gray-900 dark:text-white">
+                      Total
                     </dt>
-                    <dd className="text-base font-medium text-gray-900 dark:text-white">
-                      $7,592.00
-                    </dd>
-                  </dl>
-                  <dl className="flex items-center justify-between gap-4">
-                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                      Savings
-                    </dt>
-                    <dd className="text-base font-medium text-green-600">
-                      -$299.00
-                    </dd>
-                  </dl>
-                  <dl className="flex items-center justify-between gap-4">
-                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                      Store Pickup
-                    </dt>
-                    <dd className="text-base font-medium text-gray-900 dark:text-white">
-                      $99
-                    </dd>
-                  </dl>
-                  <dl className="flex items-center justify-between gap-4">
-                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                      Tax
-                    </dt>
-                    <dd className="text-base font-medium text-gray-900 dark:text-white">
-                      $799
+                    <dd className="text-base font-bold text-gray-900 dark:text-white">
+                      ${getTotalPrice()}
                     </dd>
                   </dl>
                 </div>
-                <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
-                  <dt className="text-base font-bold text-gray-900 dark:text-white">
-                    Total
-                  </dt>
-                  <dd className="text-base font-bold text-gray-900 dark:text-white">
-                    $8,191.00
-                  </dd>
-                </dl>
               </div>
               {/* check out bnt */}
               <Link
@@ -213,9 +193,9 @@ const CheckOut = () => {
                   {" "}
                   or{" "}
                 </span>
-                <a
-                  href="#"
-                  title=""
+                <Link
+                  to="/"
+
                   className="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
                 >
                   Continue Shopping
@@ -234,7 +214,7 @@ const CheckOut = () => {
                       d="M19 12H5m14 0-4 4m4-4-4-4"
                     />
                   </svg>
-                </a>
+                </Link>
               </div>
             </div>
 
