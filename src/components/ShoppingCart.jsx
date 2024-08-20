6ce14045d62df4fdba896c78868c5ccf93d27f07
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-19 17:46:03 -0700

    feat(shopingCartBtn): :boom: add logic to handleCart function

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -4,10 +4,13 @@ import { userContext } from '@/context/UserContext'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { CartContext } from '@/context/ShoppingCartContext';
 
 
-function ShoppingCart({ className, imgWidth }) {
+function ShoppingCart({ className, imgWidth, product }) {
 
+  const { addToCart } = useContext(CartContext);
+  
   const navigate = useNavigate()
 
   const { token } = useContext(userContext)
@@ -29,6 +32,8 @@ function ShoppingCart({ className, imgWidth }) {
       }, 3000)
       return;  // prevent adding items to cart when not logged in  //* replace alert with toast notification in production code  //* implement authentication or login functionality when not logged in
     } else {
+
+    
       toast.success('Item has been added', {
         position: "bottom-right",
         autoClose: 5000,
@@ -41,14 +46,14 @@ function ShoppingCart({ className, imgWidth }) {
 
       });
 
-      // TODO : logic to handle cart 
-
+      // Add the item to the cart here
+      addToCart(product) // product comes from Cart component props
 
     }
   }
   return (
     <>
-      <Link
+      <button
 
         className={className}
         onClick={handleCart}
@@ -59,7 +64,7 @@ function ShoppingCart({ className, imgWidth }) {
           src={shoppinCartIcon}
           alt="Shopping cart icon" />
         Add to Cart
-      </Link>
+      </button>
 
     </>
   )
