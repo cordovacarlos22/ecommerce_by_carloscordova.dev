31bd979a216a5321eb0b0ad3cf5cbdb10d662e79
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-21 15:06:45 -0700

    feat: :sparkles: introduce loading spinner on load

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,15 @@
 import CreateItem from "@/components/CreateItem";
 import GetAllUsers from "@/components/GetAllUsers";
-import React, { useState } from "react";
-import { NavLink,Link } from "react-router-dom";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import { userContext } from "@/context/UserContext";
+import React, { useContext, useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 
 
 const Dashboard = () => {
   const [activeComponent, setActiveComponent] = useState("home");
+
+  const { loading } = useContext(userContext);
 
   const renderComponent = () => {
     switch (activeComponent) {
@@ -31,43 +35,51 @@ const Dashboard = () => {
   };
 
   return (
-    <section className="flex flex-col md:flex-row  bg-gray-200  ">
-      <br />
-      <div className=" bg-blue-700 md:min-h-screen flex flex-col justify-center items-center gap-4 p-4">
-        <Link
-        to='/dashboard'
-        
-        >
-          <h1 className="font-bold  text-white">Dashboard</h1>
-        </Link>
-        <nav className="flex flex-col text-white ">
-          <button
-            className={` p-2 ${activeComponent === "createItem"
-                ? "text-blue-500 underline bg-white rounded"
-                : "text-white"
-              }`}
-            onClick={() => setActiveComponent("createItem")}>
-            Create Item
-          </button>
+    <>
+      {loading ? (<>
+        <LoadingSpinner />
+      </>) : (
+        <>
+          <section className="flex flex-col md:flex-row  bg-gray-200  ">
+            <br />
+            <div className=" bg-blue-700 md:min-h-screen flex flex-col justify-center items-center gap-4 p-4">
+              <Link
+                to='/dashboard'
 
-          <button
-            className={` p-2 ${activeComponent === "getAllUsers"
-              ? "text-blue-500 underline bg-white rounded"
-              : "text-white"
-              }`}
-            onClick={() => setActiveComponent("getAllUsers")}>
-            list all  users
-          </button>
-         
-          {/* Add more buttons for other components */}
-        </nav>
+              >
+                <h1 className="font-bold  text-white">Dashboard</h1>
+              </Link>
+              <nav className="flex flex-col text-white ">
+                <button
+                  className={` p-2 ${activeComponent === "createItem"
+                    ? "text-blue-500 underline bg-white rounded"
+                    : "text-white"
+                    }`}
+                  onClick={() => setActiveComponent("createItem")}>
+                  Create Item
+                </button>
 
-      </div>
-      <div className="flex flex-col justify-center items-center m-auto">
-        {/* This will render the component based on the active state */}
-        {renderComponent()}
-      </div>
-    </section>
+                <button
+                  className={` p-2 ${activeComponent === "getAllUsers"
+                    ? "text-blue-500 underline bg-white rounded"
+                    : "text-white"
+                    }`}
+                  onClick={() => setActiveComponent("getAllUsers")}>
+                  list all  users
+                </button>
+
+                {/* Add more buttons for other components */}
+              </nav>
+
+            </div>
+            <div className="flex flex-col justify-center items-center m-auto">
+              {/* This will render the component based on the active state */}
+              {renderComponent()}
+            </div>
+          </section>
+        </>
+      )}
+    </>
   );
 };
 
